c95278bfd10d2a08eaaf6c07ba00de5cf4944fb2
Silas Peters <silaspeters03@gmail.com>
2022-05-12 16:58:07 +0200

    Yoinkers @ Sticky

---
+++ CustomClasses.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raytracer
+{
+    internal class CustomClasses
+    {
+        public struct Ray
+        {
+
+        }
+
+        public struct LightRay : Ray { }
+
+    }
+}
