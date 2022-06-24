f4d921725c6a5ccf836c733e6bf28f6364cbbcba
Silas Peters <silaspeters03@gmail.com>
2022-06-24 10:26:27 +0200

    Cool :)

--- P2/shaders/fs.glsl
+++ P2/shaders/fs.glsl
@@ -1,21 +1,4 @@
 ﻿#version 330
-/*
-//old code
-
-// shader input
-in vec2 uv;			// interpolated texture coordinates
-in vec4 normal;			// interpolated normal
-uniform sampler2D pixels;	// texture sampler
-
-// shader output
-out vec4 outputColor;
-
-// fragment shader
-void main()
-{
-    outputColor = texture( pixels, uv ) + 0.5f * vec4( normal.xyz, 1 );
-}
-*/
 
 // inputs
 in vec4 position; // world space
@@ -26,14 +9,26 @@ uniform vec3 camPos; // world space
 uniform vec3 lightColor; // RGB
 uniform vec3 ambientLight; // amb light
 uniform sampler2D tex; // diffuse color
+uniform vec3 specularCo;
+
+const float glosyness = 50;
+
 // output
 out vec4 color; // RGBA
-// fragment shader // swizzle syntax: select, reorder, or duplicate coordinates using .xyzw or .rgba
-void main() // e.g., position.xyz, lightColor.gg, position.x
+
+
+// fragment shader
+void main()
 {
-    vec3 L = lightPosition - position.xyz; // vector from surface to light, unnormalized!
-    float attenuation = 1.0 / dot(L, L); // distance attenuation
+    vec3 toLight = lightPosition - position.xyz; // vector from surface to light, unnormalized!
+    float attenuation = 1.0 / dot(toLight, toLight); // distance attenuation
     float NdotL = max(0, dot(normalize(normal.xyz), normalize(L))); // incoming angle attenuation
     vec3 diffuseColor = texture(tex, uv).rgb; // texture lookup
-    color = vec4(lightColor * diffuseColor * attenuation * NdotL + ambientLight, 1.0 ); // complete diffuse shading, A = 1.0 is opaque
+    //color = vec4(lightColor * diffuseColor * attenuation * NdotL + ambientLight, 1.0 ); // complete diffuse shading, A = 1.0 is opaque
+
+    vec3 V = normalize(camPos - position.xyz); //todo: multiple ls
+    vec3 R = normalize(toLight - 2 * dot(normalize(toLight), normal) * normal);
+    color = vec4(attenuation * lightColor * diffuseCo * NdotL
+                + specularCo * pow(max(0, dot(V, R)), glossyness) + ambientLight
+    , 1.0);
 }
--- P2/shaders/vs.glsl
+++ P2/shaders/vs.glsl
@@ -1,28 +1,5 @@
 ﻿#version 330
-/*
-// source: 2014 lecture slides
- 
-// shader input
-in vec2 vUV;			// vertex uv coordinate
-in vec3 vNormal;		// untransformed vertex normal
-in vec3 vPosition;		// untransformed vertex position
 
-// shader output
-out vec4 normal;		// transformed vertex normal
-out vec2 uv;				
-uniform mat4 transform;
- 
-// vertex shader
-void main()
-{
-	// transform vertex using supplied matrix
-	gl_Position = transform * vec4(vPosition, 1.0);
-
-	// forward normal and uv coordinate; will be interpolated over triangle
-	normal = transform * vec4( vNormal, 0.0f );
-	uv = vUV;
-}
-*/
 // inputs
 in vec3 vPosition; // object space
 in vec3 vNormal; // object space
@@ -33,6 +10,7 @@ uniform mat4 objectToWorld;
 out vec4 position; // world space
 out vec4 normal; // world space
 out vec2 uv;
+
 // vertex shader
 void main()
 {
